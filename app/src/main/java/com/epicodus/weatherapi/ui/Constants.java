9fdbe483b82a93bb9c667c5a4bd23e8b61b57f80
Megan <mefayer@gmail.com>
2016-04-25 16:01:20 -0700

    create constants file and initial commit

---
+++ app/src/main/java/com/epicodus/weatherapi/ui/Constants.java
@@ -0,0 +1,10 @@
+package com.epicodus.weatherapi.ui;
+
+import com.epicodus.weatherapi.BuildConfig;
+
+/**
+ * Created by Guest on 4/25/16.
+ */
+public class Constants {
+    public static final String OPEN_WEATHER_CONSUMER_KEY = BuildConfig.OPEN_WEATHER_CONSUMER_KEY;
+}
